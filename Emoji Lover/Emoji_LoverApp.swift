28561ffe0f94fd186f20fe37cb183af56c80d938
dgotal <dgotal@etfos.hr>
2025-11-13 14:04:42 +0100

    Initial Commit

---
+++ Emoji Lover/Emoji_LoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_LoverApp.swift
+//  Emoji Lover
+//
+//  Created by Davor Gotal on 13.11.2025..
+//
+
+import SwiftUI
+
+@main
+struct Emoji_LoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Emoji LoverTests/Emoji_LoverTests.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_LoverTests.swift
+//  Emoji LoverTests
+//
+//  Created by Davor Gotal on 13.11.2025..
+//
+
+import Testing
+@testable import Emoji_Lover
+
+struct Emoji_LoverTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
